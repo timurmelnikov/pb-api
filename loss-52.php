cb2e814b0c7fb3ac3cc2272b515288a459674189
Timur Melnikov <melnikovt@gmail.com>
2018-04-03 09:31:56 +0300

    *

--- loss-52.php
+++ loss-52.php
@@ -28,13 +28,13 @@ curl_setopt($curl, CURLOPT_HTTPHEADER, array(
 curl_setopt($curl, CURLOPT_POSTFIELDS, '{
     "opCode": "setUsIdHandle",
     "request": {
-        "req_ID": "Z181QPB70000EM"
+        "req_ID": "Z1832PB70000FM"
     },
     "actionLst": [
         {
             "ria_Type": "k",
-            "ria_Comment": "Кудько Николай Витальевич +380676274424",
-	        "ria_DatE": "2018-03-29T00:00:00.000"
+            "ria_Comment": "Семенюк Елена Александровна +380672231169",
+	        "ria_DatE": "2018-04-03T00:00:00.000"
         }
     ]
 }');
